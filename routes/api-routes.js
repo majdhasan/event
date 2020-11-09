1ae38f31e7379a763617e79fbd9303b254ca04a6
Majd Hasan <majd.hasan@aperto.com>
2020-11-09 20:09:08 +0100

    add token authorization login

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -10,34 +10,62 @@ const Invite = require('../model/Invite');
 const { findByIdAndUpdate } = require('../model/Event');
 const Conversation = require('../model/Conversation');
 const Message = require('../model/Message');
+var jwt = require('jsonwebtoken');
 
 const apiRouter = express.Router();
 
+// ------------------------------------ Utilities ----------------------------------------
+
+const verifyToken = (req, res, next) => {
+  const bearerHeader = req.headers['authorization'];
+  if (typeof bearerHeader !== 'undefined') {
+    const [bearer, token] = bearerHeader.split(' ');
+    console.log('a request with following token has just arrived' + token);
+    jwt.verify(token, process.env.PRIVATEKEY, (err, decoded) => {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        req.token = token;
+        req.user = decoded.user;
+        next();
+      }
+    });
+  } else {
+    res.sendStatus(403);
+  }
+};
+
+// ------------------------------------ Routes ----------------------------------------
+
+// ------------------------------------ Authentication/Authorization ----------------------------------------
+
 apiRouter.route('/login').post((req, res) => {
-  let user = new User({
-    username: req.body.username,
-    password: req.body.password,
-  });
-  req.login(user, (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      passport.authenticate('local')(req, res, (err, scc) => {
-        err
-          ? console.log(err)
-          : res.send({
-              username: req.user.username,
-              firstname: req.user.firstname,
-              lastname: req.user.lastname,
-              id: req.user._id,
-            });
-      });
-    }
-  });
+  if (req.body.username && req.body.password) {
+    let user = new User({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    req.login(user, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        passport.authenticate('local')(req, res, () => {
+          jwt.sign({ user: req.user }, process.env.PRIVATEKEY, (err, token) => {
+            if (err) console.log(err);
+            if (token) {
+              res.status(200).json({ token: token, user: req.user });
+            }
+          });
+        });
+      }
+    });
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 apiRouter.route('/signup').post((req, res) => {
-    console.log(req.body);
+  console.log(req.body);
   User.register(
     {
       username: req.body.username,
@@ -52,20 +80,24 @@ apiRouter.route('/signup').post((req, res) => {
         console.log(err);
       } else {
         console.log('new user has been created' + result);
-        passport.authenticate('local')(req, res, () => {
-          res.status(200).send(req.user);
+        jwt.sign({ user: result }, process.env.PRIVATEKEY, (err, token) => {
+          if (err) console.log(err);
+          if (token) {
+            console.log(token);
+            res.json({ token });
+          }
         });
       }
     }
   );
 });
 
-apiRouter.route('/loggedUser').get((req, res) => {
-  if (req.isAuthenticated()) {
-    res.send(req.user);
-  } else {
-    res.status(400).send('You are not logged in');
-  }
+// ------------------------------------ Restricted Routes ----------------------------------------
+
+apiRouter.route('/test').get(verifyToken, (req, res) => {
+  console.log('a new request arrived from' + req.user._id);
+  res.json({ user: req.user });
 });
 
+// ------------------------------------ END OF API ----------------------------------------
 module.exports = apiRouter;
